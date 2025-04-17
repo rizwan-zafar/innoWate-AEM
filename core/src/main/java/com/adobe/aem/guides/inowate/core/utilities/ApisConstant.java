53e73aa203a9a5ee8982756664e57edd30941639
rizwan-zafar <rizwanzafar4323@gmail.com>
2025-04-17 12:13:51 +0500

    contact us component added

--- core/src/main/java/com/adobe/aem/guides/inowate/core/utilities/ApisConstant.java
+++ core/src/main/java/com/adobe/aem/guides/inowate/core/utilities/ApisConstant.java
@@ -3,7 +3,7 @@ package com.adobe.aem.guides.inowate.core.utilities;
 public class ApisConstant {
 
     public static final String FETCH_CLIENT_LOGOS = "/api/client-logo?_format=json";
-    public static final String FETCH_SERVICES_CF = "/content/cq:graphql/global/endpoint.json";
+    public static final String FETCH_SERVICES_CF = "/content/cq:graphql/inowate/endpoint.json";
     public static final String FETCH_PORTFOLIO="/api/portfolio-projects?_format=json";
     
 }
